9998132348f247db58ef66f277b3d287f2392413
D Chinmay <zchinmayz@gmail.com>
2025-11-19 22:41:47 +0530

    temp

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 
 const myServer = http.createServer((req, res) =>{
     const log = `${Date.now()} : ${req.url} new req Reaceived\n`
+    if (req.url === "/favicon.ico") return res.end();
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl)
+
+
     fs.appendFile("log.txt", log, (err, data) =>{
-        switch (req.url) {
+
+
+        switch (myUrl.pathname) {
             case "/":
-                res.end("Home")
+                res.end("HomePage")
                 break;
             
             case "/about":
-                res.end("Hey this is Chinmay")
+                const username = myUrl.query.myname
+                console.log(username)
+                res.end(`Hey there ${username}`)
                 break;
+            case "/search":
+                const search = myUrl.query.searchquery
+                console.log(search)
+                res.end(`Your results for ${search}`)
+            break;
         
             default:
                 res.end("404: Not Found")
