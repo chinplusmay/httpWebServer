5260365b759a50a178778c64b517ea09b67a23e9
D Chinmay <zchinmayz@gmail.com>
2025-11-22 20:06:59 +0530

    express+node ex

--- index.js
+++ index.js
@@ -1,44 +1,17 @@
-const http = require("http");
 const fs = require("fs");
 const url = require("url");
 const express = require("express");
 
+const app = express(); //handlerfn (instance)
 
-function handlerFn(req, res){
-    const log = `${Date.now()} : ${req.url} new req Reaceived\n`
-    if (req.url === "/favicon.ico") return res.end();
-    const myUrl = url.parse(req.url, true);
-    console.log(myUrl)
+app.get("/", (req, res) =>{
+    res.send("Hello Form homi")
+})
+
+app.get("/about", (req, res) =>{
+    res.send(`Hey ${req.query.name}`)
+})
 
 
-    fs.appendFile("log.txt", log, (err, data) =>{
 
-
-        switch (myUrl.pathname) {
-            case "/":
-                res.end("HomePage")
-                break;
-            
-            case "/about":
-                const username = myUrl.query.myname
-                console.log(username)
-                res.end(`Hey there ${username}`)
-                break;
-            case "/search":
-                const search = myUrl.query.searchquery
-                console.log(search)
-                res.end(`Your results for ${search}`)
-            break;
-        
-            default:
-                res.end("404: Not Found")
-                break;
-        }
-        
-    })
-    // console.log(req.headers)
-}
-
-const myServer = http.createServer(handlerFn)
-
-myServer.listen("8000", () => console.log("server started"))
+app.listen("8000", () => console.log("server started"))
