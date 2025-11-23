05dc591bd1c310ef33f1d52fb88218280cb9d465
D Chinmay <zchinmayz@gmail.com>
2025-11-23 11:34:43 +0530

    api routing

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-const fs = require("fs");
-const url = require("url");
 const express = require("express");
+const PORT = 8000
+const app = express();
+const users = require("./MOCK_DATA.json")
 
-const app = express(); //handlerfn (instance)
 
-app.get("/", (req, res) =>{
-    res.send("Hello Form homi")
+//REST API
+
+
+app.get("/api/users", (req, res) =>{
+    return res.json(users)
+    
 })
 
-app.get("/about", (req, res) =>{
-    res.send(`Hey ${req.query.name}`)
+app
+    .route("/api/users/:id")
+    .get((req, res) =>{
+        const id = Number(req.params.id)
+        const user = users.find((user) => user.id === id)
+        return res.json(user)
+
+    .post((req, res) =>{
+        //todo create new user
+        return res.json({status:"pending"});
+    })
+    
+    .put((req, res) =>{
+        //todo edit user all details
+        return res.json({status:"pending"});
+    })
+    .patch((req, res) =>{
+        //todo edit user name
+        return res.json({status:"pending"});
+    })
+    .delete((req, res) =>{
+        //todo delete user
+        return res.json({status:"pending"});
+    })
 })
 
+app.get("/api/users/:id", )
 
 
-app.listen("8000", () => console.log("server started"))
+app.listen(PORT, () => console.log("server started"))
