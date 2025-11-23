abf622048239921c26db43805c2198efb8a1f238
D Chinmay <zchinmayz@gmail.com>
2025-11-23 20:12:34 +0530

    post, put, patch, delete

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const fs = require("fs");
 const PORT = 8000
 const app = express();
 const users = require("./MOCK_DATA.json")
 
 
+//middleware-plugin
+
+app.use(express.urlencoded({extended:false}))
+
 //REST API
-
-
 app.get("/api/users", (req, res) =>{
     return res.json(users)
     
+})
+
+app.post("/api/users", (req, res) =>{
+    const body = req.body;
+    // console.log("Body", body)
+    users.push({id: users.length+1, ...body})
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users), (err, data) =>{
+        return res.json({status:"success", id:users.length});
+    })
 })
 
 app
@@ -18,27 +30,51 @@ app
         const id = Number(req.params.id)
         const user = users.find((user) => user.id === id)
         return res.json(user)
-
-    .post((req, res) =>{
-        //todo create new user
-        return res.json({status:"pending"});
     })
-    
     .put((req, res) =>{
         //todo edit user all details
-        return res.json({status:"pending"});
+        const body = req.body;
+        const id = Number(req.params.id)
+        const index = users.findIndex((u) => u.id === id)
+        if(index === -1) return res.status(404).json({error:"user not found"});
+
+        users[index] = {id, ...body}
+
+        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) =>{
+            return res.json({status:"user updated", id:id});
+        })
+
     })
+
     .patch((req, res) =>{
-        //todo edit user name
-        return res.json({status:"pending"});
+        //edit some part of user
+        const body = req.body;
+        const id = Number(req.params.id)
+        const user = users.find((u) => u.id === id)
+        if (!user) return res.status(404).json({ error: "User not found" });
+
+        Object.assign(user, body)
+
+        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) =>{
+            return res.json({ status: "User partially updated", user });
+        })
     })
     .delete((req, res) =>{
         //todo delete user
-        return res.json({status:"pending"});
-    })
-})
+        const body = req.body;
+        const id = Number(req.params.id)
 
-app.get("/api/users/:id", )
+        const index = users.findIndex((u) => u.id === id)
+        if (index === -1) return res.status(404).json({error:"user not found"});
+        
+        const deletetedUser = users.splice(index, 1)[0];
+
+        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) =>{
+            return res.json({status:"user deleted", deletetedUser});
+        })
+
+    });
+
 
 
 app.listen(PORT, () => console.log("server started"))
